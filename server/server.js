9e11528bf7bd6acc2f000bfa895e212d152bd43a
Muhammad Shahzad <shahzadthathal@gmail.com>
2020-09-26 23:18:06 +0400

    App deployed on Heroku server successfully

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
+
 app.listen(port, () => {
    console.log('Server is up!');
 });
