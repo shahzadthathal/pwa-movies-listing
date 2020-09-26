f3af1a7f8b8df7372a394678d5eb642a14b46030
Muhammad Shahzad <shahzadthathal@gmail.com>
2020-09-27 00:30:42 +0400

    Register form created

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Register from './components/Register/Register';
 import Header from './components/Header/Header';
 
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
 
-            <Header />
-    
-    </div>
+          <Header />
+
+          <div className="container d-flex align-items-center flex-column">
+          <Switch>
+            <Route path="/" exact={true}>
+              <Register />
+            </Route>
+          </Switch>
+       </div>
+       
+          
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,28 @@
 import React from 'react';
 function Header() {
     return(
-        <nav class="navbar navbar-dark bg-primary">
-            <div className="row col-12 d-flex justify-content-center text-white">
-            <span className="h3">Register</span>
-            </div>
-        </nav>
+       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		  <a class="navbar-brand" href="#">PWA Movies Listing</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarText">
+		    <ul class="navbar-nav mr-auto">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Signup</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Login</a>
+		      </li>
+		    </ul>
+		    <span class="navbar-text">
+		      Navbar text with an inline element
+		    </span>
+		  </div>
+		</nav>
     )
 }
 export default Header;
