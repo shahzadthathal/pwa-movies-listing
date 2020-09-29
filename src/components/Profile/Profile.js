20435a1625f49ea34b9253309634659f9dae8c60
Muhammad Shahzad <shahzadthathal@gmail.com>
2020-09-30 01:01:40 +0400

    Profile component designed

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -18,6 +18,9 @@ import {
     CardBody
 } from 'reactstrap';
 
+import './Profile.css';
+
+
 function Profile(props) {
   const [state , setState] = useState({
         full_name : "",
@@ -33,10 +36,14 @@ function Profile(props) {
               console.log("Loged in user data")
               console.log(response.data.full_name)
 
-             
-              setState({'full_name' : response.data.full_name})
-              setState({'email' : response.data.email})
 
+              //This method will rerender dom after updating value
+              setState({
+                'full_name' : response.data.full_name,
+                'email' : response.data.email
+              })
+
+              //Don't do this, this method will not rerender dom
               //state.full_name = response.data.full_name ;
               //state.email = response.data.email ;
             }
@@ -56,11 +63,68 @@ function Profile(props) {
                 <BreadcrumbItem> <a href="/">Home</a> </BreadcrumbItem>
                 <BreadcrumbItem active>Profile</BreadcrumbItem>
             </Breadcrumb>
-       
-            <h1 className="mt-2">Welcome again {state.full_name}</h1>
-            <p>Your email is: {state.email} </p>
 
-         </Container>
+            <div class="content">
+              <div class="row profile">
+                <div class="col-md-3">
+                  <div class="profile-sidebar mb-2">
+                    
+                    <div class="profile-userpic">
+                      <img src="https://media.rockstargames.com/chinatownwars/global/downloads/avatars/zhou_256x256.jpg" class="img-responsive" alt=""/>
+                    </div>
+                   
+                    <div class="profile-usertitle">
+                      <div class="profile-usertitle-name">
+                        {state.full_name}
+                      </div>
+                      <div class="profile-usertitle-job">
+                        Developer
+                      </div>
+                    </div>
+                   
+                    <div class="profile-userbuttons">
+                      <button type="button" class="btn btn-success btn-sm">Follow</button>
+                      <button type="button" class="btn btn-danger btn-sm">Message</button>
+                    </div>
+                   
+                    <div class="profile-usermenu sidebar-sticky">
+                      <ul class="nav flex-column">
+                        <li class="active nav-item">
+                          <a href="#" class="nav-link active">
+                        <i class="fa fa-home"></i>
+                        Overview </a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="https://codepen.io/jasondavis/pen/jVRwaG?editors=1000">
+                        <i class="fa fa-user"></i>
+                        Account Settings </a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#" target="_blank">
+                        <i class="fa fa-check"></i>
+                        Favourite Movies </a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">
+                        <i class="fa fa-flag"></i>
+                        Watch Later Movies</a>
+                        </li>
+                      </ul>
+                    </div>
+                    
+                  </div>
+                </div>
+                <div class="col-md-9">
+                  <div class="profile-content mb-2">
+                    Some user related content goes here...
+                    <h2>{state.email} </h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+        </Container>
+
     )
 }
 
